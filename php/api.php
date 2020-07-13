ff628140effa06a984e69a04074a7bd32d131ba8
Jean <jean_wylmer@hotmail.com>
2020-07-13 17:53:36 -0300

    fix notice in api.php file

--- php/api.php
+++ php/api.php
@@ -1,22 +1,20 @@
-
 <?php
-
 //array that contains the information needed to calculate the R0 and record the date
-
 $info = array(
-    "date" => NULL,
-    "casesStart" => NULL,
-    "casesEnd" => NULL,
+    "date" => null,
+    "casesStart" => null,
+    "casesEnd" => null,
 );
 
 
 //gets a raw csv file and manipulates it to get the wanted information to proceed
 //(the specific format of the csv is already excpected)
-function csvToData( $cache , &$info) {
+function csvToData($cache, &$info)
+{
 
     //see how much line there is on the file in order to get, at the same reading, both dates needed
     $count = 0;
-    while(!feof($cache)){
+    while (!feof($cache)) {
         $line = fgets($cache);
         $count++;
     }
@@ -24,7 +22,7 @@ function csvToData( $cache , &$info) {
     //reading stream goes back to the beggining of the file
     rewind($cache);
 
-    $buf;
+    $buf = null;
     $row_count = 0;
     $field_count = 0;
 
@@ -35,61 +33,56 @@ function csvToData( $cache , &$info) {
         $field_count = 0;
         $row_count++;
 
-        $field = strtok($buf, ",");//divide each line into tokens divided by ",", necessary to handle with .csv
+        $field = strtok($buf, ","); //divide each line into tokens divided by ",", necessary to handle with .csv
         while ($field) {
-
-
             //we count the field to know if we are reading a date or a cases toll
             if ($field_count == 0) {
                 // we check if it is a field of the latest day
-                if ($row_count == $count){
-                    $info["date"] = substr ($field, 5 , 5 ); //just the month and day
+                if ($row_count == $count) {
+                    $info["date"] = substr($field, 5, 5); //just the month and day
                 }
-
             }
 
             if ($field_count == 1) {
-            //this is a cases toll field
+                //this is a cases toll field
 
-                if($row_count == ($count - 6)){//from the previous week
+                if ($row_count == ($count - 6)) { //from the previous week
                     $info["casesStart"] = $field;
-                } else if ($row_count == $count){//from the latest day
-                   $info["casesEnd"] = $field;
-                }              
+                } elseif ($row_count == $count) { //from the latest day
+                    $info["casesEnd"] = $field;
+                }
             }
 
 
             $field = strtok(",");
 
-            $field_count++;   
-
+            $field_count++;
         }
     }
-
 }
 
 //calculating r0 based on this brasilian article calculus:
 // https://hal.archives-ouvertes.fr/hal-02509142v2/file/epidemie_pt.pdf
-function calculateR0($info){
-    
-    $initial_cases = $info["casesStart"];
-    $final_cases = $info["casesEnd"];
+function calculateR0($info)
+{
 
+    $initial_cases = (float) $info["casesStart"];
+    $final_cases = (float) $info["casesEnd"];
     $r0 = $final_cases / $initial_cases;
     $r0 = log($r0);
     $r0 = $r0 / 7;
-    $r0 = (1 + $r0/0.25) * ($r0 + 1);
+    $r0 = (1 + $r0 / 0.25) * ($r0 + 1);
 
     return $r0;
 }
 
 
-function getR0(){
-
+function getR0()
+{
     //first, open the url provided by the town hall with the wanted .csv file
     //opens also a cache resource in order to rewind the buffer ( resources of external url are not able to do so)
     //copy the url stream to the cache stream
-    //then $cache will be at the end of the stream after calling stream_copy_to_stream, so we rewind it 
+    //then $cache will be at the end of the stream after calling stream_copy_to_stream, so we rewind it
     $remoteResource = fopen("http://servicos.sorocaba.sp.gov.br/metabase/public/question/c49a0ea6-c617-491c-81d8-3862bcc639f7.csv", "r");
     $cache = fopen('php://temp', 'r+');
     stream_copy_to_stream($remoteResource, $cache);
@@ -115,16 +108,17 @@ function getR0(){
 }
 
 
-function store_r0($r0, $info){
+function store_r0($r0, $info)
+{
 
     $url = 'r0_values.json'; // path to JSON file
+    $data = new \stdClass;
     $data = file_get_contents($url); // put the contents of the file into a variable
     $data = json_decode($data); // decode the JSON feed
- 
-    $n = $data->n_stored_values; // get the number of previous values calculated in order to add one more at the end of the .json 
-    
-    if($data->r0[$n]->date == $info['date']){ // just store the newest values if it is a new day
 
+    $n = $data->n_stored_values; // get the number of previous values calculated in order to add one more at the end of the .json
+
+    if ($data->r0[$n - 1]->date != $info['date']) { // just store the newest values if it is a new day
         $data->r0[$n]->date = $info['date']; // goes to the list that contains previous values and add the newest date in the end
 
         $data->r0[$n]->value = $r0; // does the same but with the r0 number
@@ -134,8 +128,4 @@ function store_r0($r0, $info){
 
         file_put_contents($url, $data); // refresh the file
     }
-
 }
-
-?>
-
--- php/site.php
+++ php/site.php
@@ -1,28 +1,32 @@
-<?php include 'api.php'; ?>
+<?php
+include 'api.php'; ?>
 <html lang="pt-br">
 
-    <head>
-        <link rel="stylesheet" type="text/css" href="../css/style.css">
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Site que calcula R0 na cidade de Sorocaba">
-        <meta name="keywords" content="sorocaba, covid, r0, infecção">
-        <title>covid Sorocaba</title>
-    </head>
+<head>
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Site que calcula R0 na cidade de Sorocaba">
+    <meta name="keywords" content="sorocaba, covid, r0, infecção">
+    <title>covid Sorocaba</title>
+</head>
 
-    <body>
-        <p id="disclaimer">Aviso: este site é amador e os dados são apenas aproximações matemáticas</p>
+<body>
+    <p id="disclaimer">Aviso: este site é amador e os dados são apenas aproximações matemáticas</p>
 
-        <h1 id="value">O R0 é <?php echo getR0(); ?>
-        </h1>
+    <h1 id="value">O R0 é <?php echo getR0(); ?>
+    </h1>
 
-        <h2>O que é o R0?</h2>
-        <p>É o número de pessoas infectadas a partir de um único indivíduo infectado por Covid-19</p>
+    <h2>O que é o R0?</h2>
+    <p>É o número de pessoas infectadas a partir de um único indivíduo infectado por Covid-19</p>
 
-        <footer>
-            <p>Contribuintes: Guilherme Milani, Gabriel Kyomen, Marcos Santana</p>
-            <p>Link para o repositório: <a href="https://github.com/GuiMilani/covid-sorocaba">GuiMilani/covid-sorocaba</a></p>
-            <p>Referência</p>
-        </footer>
-    </body>
+    <footer>
+        <p>Contribuintes: Guilherme Milani, Gabriel Kyomen, Marcos Santana</p>
+        <p>Link para o repositório:
+            <a href="https://github.com/GuiMilani/covid-sorocaba">GuiMilani/covid-sorocaba</a>
+        </p>
+        <p>Referência</p>
+    </footer>
+</body>
+
 </html>
